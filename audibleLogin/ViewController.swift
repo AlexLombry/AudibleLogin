7b572dde302a0cb1c4ecf4bb91b15bf624d382b7
Alex Lombry <alex.lombry@gmail.com>
2016-09-26 10:48:52 +0200

    Cleanup, create extensions swift file

--- audibleLogin/ViewController.swift
+++ audibleLogin/ViewController.swift
@@ -53,14 +53,12 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         return coll
     }()
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(collectionView)
-        // taille de la collection view, celle de la page
-        //collectionView.frame = view.frame
         
-        //utilisation autolayout a la place (scale)
+        view.addSubview(collectionView)
+        
+        // place the collection view
         collectionView.anchorToTop(
             top: view.topAnchor,
             left: view.leftAnchor,
@@ -71,8 +69,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         collectionView.register(PageCell.self, forCellWithReuseIdentifier: cellFirst)
     }
 
-    
-    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellFirst, for: indexPath) as! PageCell
         let page = pages[indexPath.item]
         
@@ -81,45 +78,14 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         return cell
     }
     
-    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return pages.count
     }
     
-    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: view.frame.width, height: view.frame.height)
     }
     
-    
-}
-
-extension UIView {
-    
-    func anchorToTop(top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil) {
-        
-        anchorWithConstantsToTop(top: top, left: left, bottom: bottom, right: right, topConstant: 0, leftConstant: 0, bottomConstant: 0, rightConstant: 0)
-    }
-    
-    func anchorWithConstantsToTop(top: NSLayoutYAxisAnchor? = nil, left: NSLayoutXAxisAnchor? = nil, bottom: NSLayoutYAxisAnchor? = nil, right: NSLayoutXAxisAnchor? = nil, topConstant: CGFloat = 0, leftConstant: CGFloat = 0, bottomConstant: CGFloat = 0, rightConstant: CGFloat = 0) {
-        
-        translatesAutoresizingMaskIntoConstraints = false
-        
-        if let top = top {
-            topAnchor.constraint(equalTo: top, constant: topConstant).isActive = true
-        }
-        
-        if let bottom = bottom {
-            bottomAnchor.constraint(equalTo: bottom, constant: -bottomConstant).isActive = true
-        }
-        
-        if let left = left {
-            leftAnchor.constraint(equalTo: left, constant: leftConstant).isActive = true
-        }
-        
-        if let right = right {
-            rightAnchor.constraint(equalTo: right, constant: -rightConstant).isActive = true
-        }
-        
-    }
     
 }
 
